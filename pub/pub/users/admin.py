a1e0b838cb80f86bd6e7c746bab0a4438de0ecb8
Maestro <amaestrutti@exelio.eu>
2024-06-19 11:42:36 +0200

    admin for user

--- pub/pub/users/admin.py
+++ pub/pub/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from pub.users.models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["email", "is_staff", "is_superuser", "is_active"]
+    search_fields = ["email", "first_name", "last_name"]
+    list_filter = ["is_staff", "is_superuser", "is_active", "email"]
